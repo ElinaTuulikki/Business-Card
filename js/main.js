95bf9b46b3035b77152b4d26bf2b367f9fd95877
ElinaTuulikki <elina_tienhaara@hotmail.com>
2026-03-09 07:08:10 +0200

    right target

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const THREE = window.MINDAR.IMAGE.THREE;
 async function startAR() {
   const mindARThreeJs = new window.MINDAR.IMAGE.MindARThree({
     container: document.body,
-    imageTargetSrc: "../../assets/targets/businessCard.mind"
+    imageTargetSrc: "../assets/targets/busnessCard.mind"
   });
 
   const { cssRenderer, renderer, cssScene, scene, camera } = mindARThreeJs;
